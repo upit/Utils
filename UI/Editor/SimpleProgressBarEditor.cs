4c4092416ab5cd1d04bb7981836ed6730dcd6ce5
Upit Alex <upitsoft@gmail.com>
2020-12-08 18:24:16 +0500

    UI.Editor

---
+++ UI/Editor/SimpleProgressBarEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+
+namespace XD.UI
+{
+    /// <summary> Класс для рисования ползунка прогрессбара в инспекторе. </summary>
+    [CustomEditor(typeof(SimpleProgressBar))]
+    public class SimpleProgressBarEditor: Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawPropertiesExcluding(serializedObject, "m_Script");
+            serializedObject.ApplyModifiedProperties();
+            SimpleProgressBar progressBar = (SimpleProgressBar) target;
+            if (!progressBar.enabled)
+                return;
+            progressBar.Progress = EditorGUILayout.Slider(progressBar.Progress, 0.0f, 1.0f);
+        }
+    }
+}
---
+++ UI/Editor/UIScalerEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace XD.UI
+{
+    [CustomEditor(typeof(UIScaler))]
+    public class UIScalerEditor : Editor
+    {
+        private static RuntimePlatform platformSwitchPopUp;
+        public override void OnInspectorGUI()
+        {
+            DrawPropertiesExcluding(serializedObject, "m_Script");
+            
+            EditorGUILayout.LabelField("Принудительное переключение");
+            platformSwitchPopUp = (RuntimePlatform)EditorGUILayout.EnumPopup(platformSwitchPopUp);
+            if (GUILayout.Button("Переключить"))
+                ((UIScaler)target).ForceScale(platformSwitchPopUp);
+            
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
