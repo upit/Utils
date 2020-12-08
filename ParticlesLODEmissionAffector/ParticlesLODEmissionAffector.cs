f97733b842fa960774d80269940be30e5eea9da7
Upit Alex <upitsoft@gmail.com>
2020-12-08 18:07:46 +0500

    ParticlesLODEmissionAffector

---
+++ ParticlesLODEmissionAffector/ParticlesLODEmissionAffector.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Util.FX
+{
+    /// <summary> @Upit Класс для плавного прекращения/возобновления генерации частиц при смене LOD. </summary>
+    public class ParticlesLODEmissionAffector : MonoBehaviour
+    {
+        private ParticleSystem.EmissionModule[] emmisions;
+
+        private void Awake()
+        {
+            var pSystem = GetComponentsInChildren<ParticleSystem>();
+            emmisions = new ParticleSystem.EmissionModule[pSystem.Length];
+            for (int i = 0; i < pSystem.Length; i++)
+                emmisions[i] = pSystem[i].emission;
+        }
+
+        private void OnBecameInvisible()
+        {
+            EnableEmmision(false);
+        }
+
+        private void OnBecameVisible()
+        {
+            EnableEmmision(true);
+        }
+
+        private void EnableEmmision(bool active)
+        {
+            for (int i = 0; i < emmisions.Length; i++)
+                emmisions[i].enabled = active;
+        }
+    }
+}
